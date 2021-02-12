8cdc8c39e8ad065cb3b0823bfc8a8235e9d866c0
Boyan <boyan.siromahov@hotmail.com>
2021-02-11 22:08:12 -0500

    heroku

--- src/main/java/Model/AddressBookController.java
+++ src/main/java/Model/AddressBookController.java
@@ -3,11 +3,14 @@
     import org.springframework.stereotype.Controller;
     import org.springframework.ui.Model;
     import org.springframework.web.bind.annotation.GetMapping;
+    import org.springframework.web.bind.annotation.RequestMapping;
+    import org.springframework.web.bind.annotation.RequestMethod;
     import org.springframework.web.bind.annotation.RequestParam;
     import Model.AddressBook;
     import Model.BuddyInfo;
 
     @Controller
+    @RequestMapping(method = RequestMethod.GET)
     public class AddressBookController {
 
         private AddressBook ab;
--- src/main/java/Model/TestingWebApplicationTest.java
+++ src/main/java/Model/TestingWebApplicationTest.java
@@ -20,9 +20,9 @@ public class TestingWebApplicationTest {
     @Autowired
     private MockMvc mockMvc;
 
-//    @Test
-//    public void shouldReturnDefaultMessage() throws Exception {
-//        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-//                .andExpect(content().string(containsString("<td>Jack</td>")));
-//    }
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("<td>Jack</td>")));
+    }
 }
